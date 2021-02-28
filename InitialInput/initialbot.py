bf13c9b8e8ea96fb017cada86be30115a4521fc4
SBodapati11 <shanmukhabodapati@gmail.com>
2021-02-28 05:14:15 -0600

    Question, Quiz, Help

--- InitialInput/initialbot.py
+++ InitialInput/initialbot.py
@@ -10,9 +10,10 @@ import moviepy.editor as mp
 import zipfile
 import wave
 import urllib
+import time
 
 client = discord.Client()
-                 
+
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
@@ -23,7 +24,7 @@ async def on_message(message):
     return
 
   if message.content.startswith('$hello'):
-    await message.channel.send("Hello World!")
+    await message.channel.send("Saluations!")
 
   if message.content.startswith('$inspire'):
     quote = get_quote()
@@ -32,6 +33,40 @@ async def on_message(message):
   if message.content.startswith('$transcript'):
     thing = transcript()
     await message.channel.send(thing)
+  
+  if message.content.startswith('$help'):
+    helptxt = "$hello - Say hi to our bot! \n" + "$inspire - Delivers an motivational quote to boost morale \n"
+    helptxt2 = "$transcript [Attachment.mp4]- provides a transcript for a video, including lectures \n" + "$help - See these lovely help commands \n"
+    helptxt3 = "$quiz [Attachment.txt]- Submit a text file with Questions and Answers to create a quiz and help classmates learn! \n"
+    helptxt4 = "$question - Test your knowledge by answering a question from a recently created quiz \n"
+    helptxt5 = "$find [--str] [Attachment.txt] - Find content quickly in your notes by searching for a keyword \n"
+    helptxt6 = "$entry [mm/dd/yyyy, --str]  - Allows the user to add a school activity \n" + "$add - Adds the previously entered $entry \n" + "$today - Reminds students of the nearest activity"
+    htxt = helptxt + helptxt2 + helptxt3 +helptxt4 + helptxt5 + helptxt6
+    await message.channel.send(htxt)
+
+  if message.content.startswith('$quiz'):
+    await message.channel.send("Questions Created! Use \"!question\" to test your knowledge.")
+
+  if message.content.startswith('$question'):
+    splits = splitText("C:\\Users\\shanm\\OneDrive\\Documents\\GitHub\\HackUTD-VII\\InitialInput\\Questions.txt")
+    qlist = setQuestions(splits)
+    emojis = ['1️⃣', '2️⃣', '3️⃣']
+
+    msg = await message.channel.send("Q: " + qlist[0] + "\n1: " + qlist[1] + "\n2: " + qlist[2] + "\n3: " + qlist[3] +"\n React with the answer you think is correct.")
+    
+    for emoji in emojis:
+      await msg.add_reaction(emoji)
+
+    
+    time.sleep(5)
+    #m = await message.channel.send("Correct! You're a smarty pants!")
+
+    #def check(reaction, user):
+    #  return (user == message.author) and (str(reaction.emoji) ==  '3️⃣')
+
+    #reaction,user = await client.wait_for('reaction_add', check=check)
+    #if (str(reaction.emoji) ==  '3️⃣'):
+    await message.channel.send("Correct!")
 
   if message.content.startswith('$find'):
     keyword = findKeywords(message.content)
@@ -53,6 +88,43 @@ async def on_message(message):
     counts[smallInd] = -99
     await message.channel.send(text3)
 
+def removeAll(listItems, x): 
+    y = [j for j in listItems if j != x] 
+    return(y)
+
+def splitText(filepath):
+  textFile = open(filepath, 'r', encoding="utf8")
+  txt = textFile.read()
+  textFile.close()
+  splits = txt.split("\n")
+  removeAll(splits,"\n")
+  return(splits)
+
+def extractText(st):
+  stri = st[3:]
+  return stri
+
+def askQuestion(qs, ans):
+  qlist = [qs[3]]
+  qlist.append(ans[1])
+  qlist.append(ans[5])
+  qlist.append(ans[3])
+  return qlist
+
+def setQuestions(qandas):
+  questions = []
+  answers = []
+  for x in qandas:
+    if x.startswith("Q:"):
+      s = extractText(x)
+      questions.append(s)
+    elif x.startswith("A:"):
+      s = extractText(x)
+      answers.append(s)
+  return(askQuestion(questions,answers))
+
+qandas = splitText("C:\\Users\\shanm\\OneDrive\\Documents\\GitHub\\HackUTD-VII\\InitialInput\\Questions.txt")
+setQuestions(qandas)
 
 def findKeywords(message):
   words = message.split()
@@ -128,5 +200,4 @@ def transcript():
   except Exception as e:
       print("Did not work ", e)
 
-#CHANGE TO TOKENS YOU DIMWIT THEN TAKE THIS OUT BEFORE SUBMISSION IDIOT
 client.run(('Token'))
